d24d0d4ab54d1e1fb24762d65fe7703ca8ddcfda
Richard Cuervo <richardcuer@hotmail.com>
2025-04-14 04:22:48 +0000

    [Primer Commit] Dieron ok todos los test.

--- src/app.py
+++ src/app.py
@@ -30,13 +30,49 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
+    response_body = members
     return jsonify(response_body), 200
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    body = request.get_json(silent=True)
+    
+    if body is None:
+        return jsonify({'msg':'Debes enviar informacion en el body'}), 400
+    if "first_name" not in body:
+        return jsonify({'msg':'El campo first_name es obligatorio'}), 400
+    if "age" not in body:
+        return jsonify({'msg':'El campo age es obligatorio'}), 400
+    if "lucky_numbers" not in body:
+        return jsonify({'msg':'El campo lucky_numbers es obligatorio'}), 400
+    
+    new_member_data ={
+        'id': body['id'] if 'id' in body else jackson_family._generateId(),
+        'first_name': body['first_name'],
+        'last_name': jackson_family.last_name,
+        'age': body['age'],
+        'lucky_numbers': body['lucky_numbers']
+    }
+    new_members = jackson_family.add_member(new_member_data)
+    return jsonify(new_members), 200
+
+@app.route('/member/<int:member_id>', methods= ['GET'])
+def get_member(member_id):
+   member = jackson_family.get_member(member_id)
+   if member is None:
+       return jsonify({"msg": "Miembro no encontrado"}), 404
+   return jsonify(member), 200
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    deleted = jackson_family.delete_member(member_id)
+    if not deleted:
+        return jsonify({"msg": "Miembro no encontrado"}), 404
+    return jsonify({"done": True}), 200
+    
+
+      
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
